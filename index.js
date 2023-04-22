0369f7d111adc960235782fdebe18e8d7a76a5fa
erjanmx <erjanmx@gmail.com>
2023-04-23 00:27:21 +0200

    Update script

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+
 const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
-const fetch = require('node-fetch');
-const fs = require('fs');
 
-async function downloadHTML(url, filename) {
-  if (fs.existsSync(filename)) {
-    console.log(`File '${filename}' already exists, skipping download`);
-    return;
-  }
 
-  const response = await fetch(url);
-  const html = await response.text();
-  fs.writeFileSync(filename, html);
-  console.log(`File '${filename}' saved`);
+function buildUrl(word) {
+    return `https://www.welklidwoord.nl/${word}`;
+}
+
+function buildFilename(word) {
+    return `pages/${word}.html`;
+}
+
+async function downloadHTML(word) {
+    const filename = buildFilename(word);
+    if (fs.existsSync(filename)) {
+        console.log(`File '${filename}' already exists, skipping download`);
+        return;
+    }
+    const response = await fetch(buildUrl(word));
+    const htmlContent = await response.text();
+
+    fs.writeFileSync(filename, htmlContent);
+    console.log(`File '${filename}' saved`);
+}
+
+async function extractArticle(word) {
+    const html = fs.readFileSync(buildFilename(word), 'utf-8');
+
+    return html.match(/\<span\>(De|Het)<\/span>\s*[^<]*<\/h2>/)?.[1];
 }
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -20,26 +37,24 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply('Hoi. Stuur me een woord en ik geef het lidwoord'));
 
 bot.on(message('text'), async (ctx) => {
-  const words = ctx.message.text.split(' ');
+    const words = ctx.message.text.split(' ');
 
-  if (words.length > 1) {
-    ctx.reply('Stuur slechts één woord');
-    return;  
-  }
-  
-  const word = words[0].replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase()
-  const url = `https://www.welklidwoord.nl/${word}`;
-  const filename = `pages/${word}.html`;
+    if (words.length > 1) {
+        ctx.reply('Stuur slechts één woord');
+        return;  
+    }
 
-  try {
-    await downloadHTML(url, filename);
-    const html = fs.readFileSync(filename, 'utf-8');
-    const value = html.match(/\<span\>(De|Het)<\/span>\s*[^<]*<\/h2>/)?.[1];
-    ctx.reply(value ? `${value} ${word}` : `Geen resultaat voor '${word}'`);
-  } catch (error) {
-    console.error(`Error processing word '${word}':`, error);
-    ctx.reply(`Sorry, er ging iets mis`);
-  }
+    const word = words[0].replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase()
+    
+    try {
+        await downloadHTML(word);
+        const value = await extractArticle(word);
+
+        ctx.reply(value ? `${value} ${word}` : `Geen resultaat voor '${word}'`);
+    } catch (error) {
+        console.error(`Error processing word '${word}':`, error);
+        ctx.reply(`Sorry, er ging iets mis`);
+    }
 });
 
 bot.launch();
