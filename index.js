4ee05fb856432cb8e809d5bb8377cb4023b1f476
Erjan Kalybek <4899432+erjanmx@users.noreply.github.com>
2023-04-26 00:21:45 +0200

    Use db (#1)
    * use db

--- index.js
+++ index.js
@@ -1,41 +1,66 @@
-const fs = require('fs');
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const sqlite3 = require('sqlite3').verbose();
 
 const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
 
+const db = new sqlite3.Database('words.db');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-function buildUrl(word) {
-    return `https://www.welklidwoord.nl/${word}`;
-}
 
-function buildFilename(word) {
-    return `pages/${word}.html`;
-}
+db.run(`CREATE TABLE IF NOT EXISTS words (word TEXT NOT NULL UNIQUE, article TEXT NOT NULL)`);
 
-async function downloadHTML(word) {
-    const filename = buildFilename(word);
-    if (fs.existsSync(filename)) {
-        console.log(`File '${filename}' already exists, skipping download`);
-        return;
-    }
-    const response = await fetch(buildUrl(word));
+
+async function getArticleFromWebsite(word) {
+    const pageUrl = `https://www.welklidwoord.nl/${word}`;
+    const response = await fetch(pageUrl);
     const htmlContent = await response.text();
 
-    fs.writeFileSync(filename, htmlContent);
-    console.log(`File '${filename}' saved`);
-}
-
-function extractArticle(word) {
-    const html = fs.readFileSync(buildFilename(word), 'utf-8');
-    const $ = cheerio.load(html);
+    const $ = cheerio.load(htmlContent);
     const article = $('h2.nieuwH2 span').first().text();
 
     return (article === 'De' || article === 'Het') ? article : null;
 }
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+async function getArticleFromDatabase(word) {
+    return new Promise((resolve, reject) => {
+        db.get('SELECT article FROM words WHERE word = ? LIMIT 1', [word], (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row ? row.article : null);
+            }
+        });
+    });
+}
+
+async function saveArticleToDatabase(word, article) {
+    return new Promise((resolve, reject) => {
+        db.run('INSERT INTO words (word, article) VALUES (?, ?)', [word, article], (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+async function processWord(word) {
+    const cleanedWord = word.replace(/[^\w\s]|_/g, '').replace(/\s+/g, '').toLowerCase();
+    
+    let article = await getArticleFromDatabase(cleanedWord);
+    
+    if (!article) {
+        article = await getArticleFromWebsite(cleanedWord);
+
+        if (article) {
+            await saveArticleToDatabase(cleanedWord, article);
+        }
+    }
+    return [ cleanedWord, article ];
+}
 
 bot.start((ctx) => ctx.reply('Hoi. Stuur me een woord en ik geef het lidwoord'));
 
@@ -47,13 +72,10 @@ bot.on(message('text'), async (ctx) => {
         return;  
     }
 
-    const word = words[0].replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase()
-    
     try {
-        await downloadHTML(word);
-        const value = await extractArticle(word);
+        const [ word, article ] = await processWord(words[0]);
 
-        ctx.reply(value ? `${value} ${word}` : `Geen resultaat voor '${word}'`);
+        ctx.reply(article ? `${article} ${word}` : `Geen resultaat voor '${word}'`);
     } catch (error) {
         console.error(`Error processing word '${word}':`, error);
         ctx.reply(`Sorry, er ging iets mis`);
